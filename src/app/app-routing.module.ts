17d826952805f159ed299f0364317ec4f23c32f1
robin-hassan <robiul.hassan12102@gmail.com>
2021-08-30 02:30:39 +0800

    added reload stategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true, enableTracing: false})],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+    enableTracing: false,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
