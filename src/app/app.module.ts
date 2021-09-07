13a93c18d3d3565c4c1fed7d557510074a6874ff
robin-hassan <robiul.hassan12102@gmail.com>
2021-09-07 22:54:46 +0800

    update details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import { Page404Component } from './page404/page404.component';
+import {Page404Component} from './page404/page404.component';
 import {HomeComponent} from "./home/home.component";
 import {ResumeComponent} from "./resume/resume.component";
 import {AboutComponent} from "./about/about.component";
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
@@ -18,7 +18,9 @@ import {MatDividerModule} from "@angular/material/divider";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatTabsModule} from "@angular/material/tabs";
-import { SectionComponent } from './section/section.component';
+import {SectionComponent} from './section/section.component';
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {ExperienceComponent} from './experience/experience.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,8 @@ import { SectionComponent } from './section/section.component';
     ResumeComponent,
     AboutComponent,
     Page404Component,
-    SectionComponent
+    SectionComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +45,8 @@ import { SectionComponent } from './section/section.component';
     MatDividerModule,
     MatFormFieldModule,
     MatInputModule,
-    MatTabsModule
+    MatTabsModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
